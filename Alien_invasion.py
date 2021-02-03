69189eea9a5d0f58ac873a7082e72267928173fe
Leexin <Leexin29@qq.com>
2021-02-03 22:00:39 +0800

    创建settings类

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -1,22 +1,25 @@
 import sys
-
 import pygame 
+from settings import Settings
 
 def run_game():         #初始化游戏并创建一个屏幕对象
-    pygame.init()
-    screen = pygame.display.set_mode((1000,700))
-    pygame.display.set_caption("Alien Invasion")
+    pygame.init()       #初始化背景设置
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))  #创建显示窗口
+    pygame.display.set_caption("Alien Invasion")  #设置窗口标题
+    background_color = (230,230,230)   #设置背景颜色
+
 
 
     #开始游戏主循环
     while True:
 
-        #监事键盘和鼠标事件
+        #监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        screen.fill(ai_settings.background_color)    #用背景颜色填充屏幕
+        pygame.display.flip()            #每次循环时都重绘屏幕，让最近绘制的屏幕可见  
 
 run_game()
