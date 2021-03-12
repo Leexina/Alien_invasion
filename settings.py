37c675bd9a98037eeb4289a80ad56cd3c028ac8e
Leexin <Leexin29@qq.com>
2021-03-13 00:46:25 +0800

    一

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
+#储存《外星人入侵》的所有设置的类
 class Settings():
+    #初始化游戏设置
     def __init__(self):
-        self.screen_width = 1000
+        self.screen_width = 1000    #屏幕设置
         self.screen_height = 700
         self.background_color = (230,230,230)
